a7ddf77856a28e882172ad0fd122087dbe832c78
Ignas Butautas <ignasbutautas7@gmail.com>
2020-07-31 16:52:25 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -20,10 +20,15 @@ while counter < cart.length
   else
     new_cart_item = {
       :item => cart[counter][:item],
+      :price => cart[counter][:price],
+      :clearance => cart[counter][:clearance],
+      :count => 1 
     }
+    new_cart << new_cart_item
   end
   counter += 1 
   end
+  new_cart
 end
 
   
